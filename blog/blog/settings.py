5ddb63f0ac9b784024ba191d83171c0fcc3a386a
TK-DS-DS <695481752@qq.com>
2021-07-19 15:34:40 +0800

    'creatproject'

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -75,7 +75,8 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST':''
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
